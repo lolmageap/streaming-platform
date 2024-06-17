8a7ee42898bd9d2d5fd66dabf245bf0e4f4fc63f
lolmageap <ekxk1234@naver.com>
2024-06-17 21:52:36 +0900

    update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,67 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
-    id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.5"
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.spring") version "2.0.0"
+    kotlin("plugin.allopen") version "2.0.0"
+    kotlin("plugin.noarg") version "2.0.0"
 }
-
-group = "com.cherhy"
-version = "0.0.1-SNAPSHOT"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+allprojects {
+    group = "com.cherhy"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
     }
 }
 
-repositories {
-    mavenCentral()
-}
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "io.spring.dependency-management")
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+    dependencies {
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+        testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+        testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.3")
+        }
+    }
+
+    tasks.withType<KotlinJvmCompile>()
+        .configureEach {
+            compilerOptions
+                .languageVersion
+                .set(
+                    org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+                )
+        }
+
+    tasks.withType<KotlinCompile> {
+        kotlin {
+            compilerOptions {
+                freeCompilerArgs.add("-Xexport-kdoc")
+                jvmTarget.set(JvmTarget.JVM_17)
+            }
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 docker {
