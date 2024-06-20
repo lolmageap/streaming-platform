554d410993a5c2e15d57a068b68bdf7cb30e23b4
lolmageap <ekxk1234@naver.com>
2024-06-20 23:22:58 +0900

    init stream module

---
+++ stream/src/test/kotlin/com/cherhy/stream/StreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cherhy.stream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
