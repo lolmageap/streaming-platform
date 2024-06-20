576b08477e1b48def1382b6099213a32621acd17
lolmageap <ekxk1234@naver.com>
2024-06-20 23:51:23 +0900

    update stream module

--- stream/build.gradle.kts
+++ stream/build.gradle.kts
@@ -1,37 +1,40 @@
 plugins {
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+	kotlin("jvm") version "2.0.0"
+	id("io.ktor.plugin") version "2.3.11"
 }
 
-group = "com.cherhy"
-version = "0.0.1-SNAPSHOT"
+application {
+	mainClass.set("io.ktor.server.netty.EngineMain")
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-repositories {
-	mavenCentral()
+	val isDevelopment: Boolean = project.ext.has("development")
+	applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("io.ktor:ktor-server-core-jvm")
+	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+	implementation("io.ktor:ktor-server-config-yaml")
+	implementation("io.ktor:ktor-serialization-jackson-jvm")
+	implementation("io.ktor:ktor-server-call-logging-jvm")
+	implementation("io.ktor:ktor-server-swagger-jvm")
+	implementation("io.ktor:ktor-server-netty-jvm")
+	implementation("io.ktor:ktor-server-websockets-jvm:2.3.2")
+	implementation("io.ktor:ktor-server-status-pages")
+	implementation("io.ktor:ktor-server-partial-content:2.3.11")
+	implementation("io.github.microutils:kotlin-logging:3.0.5")
+
+	implementation("io.minio:minio:8.5.10")
+	implementation("org.postgresql:postgresql:42.7.3")
+	implementation("com.zaxxer:HikariCP:5.1.0")
+	implementation("io.ktor:ktor-server-websockets")
+
+	implementation("org.slf4j:slf4j-simple:2.0.13")
+	implementation("io.insert-koin:koin-ktor:3.5.6")
+
+	testImplementation("io.ktor:ktor-server-tests-jvm")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
 }
