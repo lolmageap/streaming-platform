1281e0468b2977bd50f285c6eade5660cd246700
lolmageap <ekxk1234@naver.com>
2024-06-21 21:42:39 +0900

    add property and setting

--- gateway/src/main/kotlin/com/cherhy/example/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/cherhy/example/GatewayApplication.kt
@@ -1,9 +1,11 @@
 package com.cherhy.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.cherhy.example.util.property")
 class GatewayApplication
 
 fun main(args: Array<String>) {
---
+++ gateway/src/main/kotlin/com/cherhy/example/util/constant/Domain.kt
@@ -0,0 +1,10 @@
+package com.cherhy.example.util.constant
+
+object Domain {
+    const val PAYMENT_SERVICE = "payment-service"
+    const val PAYMENT_SERVICE_PATH = "/payment/**"
+    const val STREAM_SERVICE = "stream-service"
+    const val STREAM_SERVICE_PATH = "/stream/**"
+    const val USER_SERVICE = "user-service"
+    const val USER_SERVICE_PATH = "/order/**"
+}
