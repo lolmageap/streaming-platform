1abf9f844775a0ceda1e995316018eace23f92b5
lolmageap <ekxk1234@naver.com>
2024-06-21 21:12:34 +0900

    init gateway module

---
+++ gateway/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
+}
+
+val coroutineVersion = "1.6.4"
+
+dependencies {
+    implementation(project(":common"))
+
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.4")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.smallrye.reactive:mutiny-kotlin:2.0.0")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.springframework.restdocs:spring-restdocs-webtestclient:3.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutineVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
