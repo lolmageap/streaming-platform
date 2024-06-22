385c2bc44f58f539529cc155ed364e259e27aaff
lolmageap <ekxk1234@naver.com>
2024-06-22 23:10:53 +0900

    register to dependency injection setting

--- user/src/main/kotlin/cherhy/example/plugins/config/DependencyInjectionConfig.kt
+++ user/src/main/kotlin/cherhy/example/plugins/config/DependencyInjectionConfig.kt
@@ -1,5 +1,11 @@
 package cherhy.example.plugins.config
 
+import cherhy.example.plugins.repository.UserRepository
+import cherhy.example.plugins.repository.UserRepositoryImpl
+import cherhy.example.plugins.service.ReadUserService
+import cherhy.example.plugins.service.WriteUserService
+import cherhy.example.plugins.usecase.LoginUseCase
+import cherhy.example.plugins.usecase.SignUpUseCase
 import cherhy.example.plugins.util.JwtManager
 import io.ktor.server.application.*
 import org.koin.dsl.module
@@ -7,6 +13,13 @@ import org.koin.ktor.plugin.Koin
 
 val dependencyInjectionModule = module {
     single { JwtManager() }
+    single { LoginUseCase(get()) }
+    single { SignUpUseCase(get(), get()) }
+
+    single { WriteUserService(get()) }
+    single { ReadUserService(get()) }
+
+    single<UserRepository> { UserRepositoryImpl() }
 }
 
 fun Application.configureDependencyInjection() {
