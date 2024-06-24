6b6fd4b0d5bd509dc37ad473e8d81eafe630a598
lolmageap <ekxk1234@naver.com>
2024-06-24 21:47:31 +0900

    add transactional to facade annotation

--- common/src/main/kotlin/com/cherhy/common/annotation/Facade.kt
+++ common/src/main/kotlin/com/cherhy/common/annotation/Facade.kt
@@ -1,9 +1,11 @@
 package com.cherhy.common.annotation
 
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @Component
+@Transactional
 annotation class Facade
