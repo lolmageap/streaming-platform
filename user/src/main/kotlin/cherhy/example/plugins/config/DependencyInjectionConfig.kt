1504abeab12ebade174481b1ce8f257d0bbf502b
lolmageap <ekxk1234@naver.com>
2024-06-24 21:50:30 +0900

    add configuration to dependency injection

--- user/src/main/kotlin/cherhy/example/plugins/config/DependencyInjectionConfig.kt
+++ user/src/main/kotlin/cherhy/example/plugins/config/DependencyInjectionConfig.kt
@@ -1,8 +1,12 @@
 package cherhy.example.plugins.config
 
+import cherhy.example.plugins.repository.AuthorityRepository
+import cherhy.example.plugins.repository.AuthorityRepositoryImpl
 import cherhy.example.plugins.repository.UserRepository
 import cherhy.example.plugins.repository.UserRepositoryImpl
+import cherhy.example.plugins.service.ReadAuthorityService
 import cherhy.example.plugins.service.ReadUserService
+import cherhy.example.plugins.service.WriteAuthorityService
 import cherhy.example.plugins.service.WriteUserService
 import cherhy.example.plugins.usecase.LoginUseCase
 import cherhy.example.plugins.usecase.SignUpUseCase
@@ -13,13 +17,16 @@ import org.koin.ktor.plugin.Koin
 
 val dependencyInjectionModule = module {
     single { JwtManager() }
-    single { LoginUseCase(get()) }
-    single { SignUpUseCase(get(), get()) }
+    single { LoginUseCase(get(), get(), get()) }
+    single { SignUpUseCase(get(), get(), get()) }
 
     single { WriteUserService(get()) }
     single { ReadUserService(get()) }
+    single { WriteAuthorityService(get()) }
+    single { ReadAuthorityService(get()) }
 
     single<UserRepository> { UserRepositoryImpl() }
+    single<AuthorityRepository> { AuthorityRepositoryImpl() }
 }
 
 fun Application.configureDependencyInjection() {
