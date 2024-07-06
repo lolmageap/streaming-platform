37307f44bd8cf7b972ac61a6408fb39ec2078ac2
lolmageap <ekxk1234@naver.com>
2024-07-06 14:54:33 +0900

    apply base entity to post entity

--- stream/src/main/kotlin/com/cherhy/plugins/domain/Post.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/domain/Post.kt
@@ -1,29 +1,24 @@
 package com.cherhy.plugins.domain
 
-import org.ktorm.entity.Entity
-import org.ktorm.schema.Table
-import org.ktorm.schema.datetime
+import com.cherhy.plugins.util.model.BaseEntity
+import com.cherhy.plugins.util.model.BaseEntityFactory
+import com.cherhy.plugins.util.model.BaseTable
 import org.ktorm.schema.long
 import org.ktorm.schema.varchar
-import java.time.ZonedDateTime
 
-object Posts : Table<Post>("post") {
+object Posts : BaseTable<Post>("post") {
     val id = long("id").primaryKey().bindTo { it.id.value }
     val title = varchar("title").bindTo { it.title.value }
     val content = varchar("content").bindTo { it.content.value }
     val author = long("author").bindTo { it.author.value }
-    val createdAt = datetime("created_at").bindTo { it.createdAt.toLocalDateTime() }
-    val updatedAt = datetime("updated_at").bindTo { it.updatedAt.toLocalDateTime() }
 }
 
-interface Post : Entity<Post> {
-    companion object : Entity.Factory<Post>()
+interface Post : BaseEntity<Post> {
+    companion object : BaseEntityFactory<Post>()
     val id: PostId
     var title: PostTitle
     var content: PostContent
     val author: UserId
-    val createdAt: ZonedDateTime
-    val updatedAt: ZonedDateTime
 }
 
 @JvmInline
