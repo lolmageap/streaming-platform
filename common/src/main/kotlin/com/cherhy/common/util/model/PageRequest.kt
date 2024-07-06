9dbf097ee23a170c897501f9dea9285b7c6e01b6
lolmageap <ekxk1234@naver.com>
2024-07-06 15:38:30 +0900

    add sort and direction to page request

--- common/src/main/kotlin/com/cherhy/common/util/model/PageRequest.kt
+++ common/src/main/kotlin/com/cherhy/common/util/model/PageRequest.kt
@@ -3,6 +3,12 @@ package com.cherhy.common.util.model
 data class PageRequest(
     val page: Int,
     val size: Int,
+    val sort: String,
+    val direction: Direction,
 ) {
     val offset = (page - 1) * size.toLong()
+}
+
+enum class Direction {
+    ASC, DESC,
 }
