c5c5756c5258741d6bf206a1d44a6d4f4fb6dd73
lolmageap <ekxk1234@naver.com>
2024-07-07 21:48:08 +0900

    create video validator

---
+++ stream/src/main/kotlin/com/cherhy/plugins/util/VideoValidator.kt
@@ -0,0 +1,15 @@
+package com.cherhy.plugins.util
+
+object VideoValidator {
+    fun validate(
+        name: String,
+        size: Long,
+    ) {
+        require(name.isNotBlank()) { "Name must not be blank" }
+        val isVideo = name.endsWith(".mp4")
+        require(isVideo) { "Invalid video format." }
+
+        require(size > 0) { "Size must be greater than 0" }
+        require(size <= 1024 * 1024 * 1024) { "Size must be less than 1GB" }
+    }
+}
