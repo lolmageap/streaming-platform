7af9c63ba6338f03baa95043c346b56b2accc733
lolmageap <ekxk1234@naver.com>
2024-07-07 21:49:20 +0900

    implement get video method

--- stream/src/main/kotlin/com/cherhy/plugins/util/extension/ApplicationCallExtension.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/util/extension/ApplicationCallExtension.kt
@@ -1,22 +1,56 @@
 package com.cherhy.plugins.util.extension
 
+import com.cherhy.plugins.api.CreateVideoRequest
 import com.cherhy.plugins.util.PathVariable
-import com.cherhy.plugins.util.model.toUserId
+import com.cherhy.plugins.util.VideoValidator
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 val mapper = jacksonObjectMapper()
 
 val ApplicationCall.pathVariable
     get() = PathVariable(this)
 
-val ApplicationCall.userId
-    get() = this.request.headers["user-id"]?.toLongOrNull()?.toUserId()
-        ?: throw IllegalArgumentException("user-id header is required")
-
 inline fun <reified T : Any> ApplicationCall.getQueryParams(): T {
     return this.request.queryParameters.toClass()
+}
+
+suspend fun ApplicationCall.getVideo(): CreateVideoRequest {
+    val multipart = this.receiveMultipart()
+
+    val videoName = multipart.readPart()
+        ?.name
+        ?: throw IllegalArgumentException("video is required.")
+
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    multipart.forEachPart { part ->
+        if (part is PartData.FileItem) {
+            part.streamProvider().use { inputStream ->
+                inputStream.copyTo(byteArrayOutputStream)
+            }
+        }
+        part.dispose()
+    }
+
+    val data = ByteArrayInputStream(
+        byteArrayOutputStream.toByteArray()
+    )
+
+    val videoSize = data.available().toLong()
+    VideoValidator.validate(videoName, videoSize)
+
+    return CreateVideoRequest.of(
+        name = videoName,
+        uniqueName = UUID.randomUUID().toString(),
+        data = data,
+        size = videoSize,
+    )
 }
 
 inline fun <reified T : Any> Parameters.toClass(): T {
