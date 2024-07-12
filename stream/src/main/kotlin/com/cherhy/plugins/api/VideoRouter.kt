bd4814b641d6fd7a882720ca25276ecfe2cf074d
lolmageap <ekxk1234@naver.com>
2024-07-12 23:57:53 +0900

    declare get video use case

--- stream/src/main/kotlin/com/cherhy/plugins/api/VideoRouter.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/api/VideoRouter.kt
@@ -1,19 +1,22 @@
 package com.cherhy.plugins.api
 
 import com.cherhy.common.util.Stream
+import com.cherhy.plugins.usecase.GetVideoUseCase
 import com.cherhy.plugins.util.extension.lastWatchedCheckpoint
 import com.cherhy.plugins.util.extension.pathVariable
 import com.cherhy.plugins.util.extension.userId
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Route.video() {
+    val getVideoUseCase: GetVideoUseCase by inject()
     get(Stream.Video.GET_VIDEO) {
         val userId = call.request.userId
         val postId = call.pathVariable.postId
         val videoId = call.pathVariable.videoId
         val lastVideoByte = call.request.lastWatchedCheckpoint
 
-        // TODO: Get video by postId, videoId, userId and lastVideoByte
+        getVideoUseCase.execute(userId, postId, videoId, lastVideoByte)
     }
 }
