02987e3dbefd59fecef0cc05095b1a2746c5a935
lolmageap <ekxk1234@naver.com>
2024-07-12 23:59:34 +0900

    create get video use case

---
+++ stream/src/main/kotlin/com/cherhy/plugins/usecase/GetVideoUseCase.kt
@@ -0,0 +1,22 @@
+package com.cherhy.plugins.usecase
+
+import com.cherhy.common.util.model.UserId
+import com.cherhy.plugins.config.MinioFactory
+import com.cherhy.plugins.domain.PostId
+import com.cherhy.plugins.domain.VideoId
+import com.cherhy.plugins.service.ReadVideoService
+
+class GetVideoUseCase(
+    private val readVideoService: ReadVideoService,
+) {
+    private val minioFactory = MinioFactory.newInstance()
+
+    suspend fun execute(
+        userId: UserId,
+        postId: PostId,
+        videoId: VideoId,
+        lastVideoByte: Byte?,
+    ) {
+        TODO()
+    }
+}
