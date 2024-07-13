e187fa0e0ca45941c6b12b6f012d6cef92052742
lolmageap <ekxk1234@naver.com>
2024-07-13 23:40:58 +0900

    implement write logic to video domain

--- stream/src/main/kotlin/com/cherhy/plugins/repository/VideoRepository.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/repository/VideoRepository.kt
@@ -1,7 +1,81 @@
 package com.cherhy.plugins.repository
 
+import com.cherhy.common.util.model.UserId
+import com.cherhy.plugins.api.VideoDetailResponse
+import com.cherhy.plugins.domain.*
+
 interface VideoRepository {
+    suspend fun save(
+        userId: UserId,
+        postId: PostId,
+        name: VideoName,
+        uniqueName: VideoUniqueName,
+        size: VideoSize,
+        extension: VideoExtension,
+    ): VideoId
+
+    suspend fun update(
+        videoId: VideoId,
+        userId: UserId,
+        name: VideoName,
+        uniqueName: VideoUniqueName,
+        size: VideoSize,
+        extension: VideoExtension
+    )
+
+    suspend fun delete(
+        userId: UserId,
+        videoId: VideoId,
+    )
+
+    suspend fun find(
+        videoId: VideoId,
+    ): VideoDetailResponse
+
+    suspend fun find(
+        postId: PostId,
+    ): VideoDetailResponse
 }
 
 class VideoRepositoryImpl : VideoRepository {
+    override suspend fun save(
+        userId: UserId,
+        postId: PostId,
+        name: VideoName,
+        uniqueName: VideoUniqueName,
+        size: VideoSize,
+        extension: VideoExtension,
+    ): VideoId {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(
+        videoId: VideoId,
+        userId: UserId,
+        name: VideoName,
+        uniqueName: VideoUniqueName,
+        size: VideoSize,
+        extension: VideoExtension
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(
+        userId: UserId,
+        videoId: VideoId,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun find(
+        videoId: VideoId,
+    ): VideoDetailResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun find(
+        postId: PostId,
+    ): VideoDetailResponse {
+        TODO("Not yet implemented")
+    }
 }
--- stream/src/main/kotlin/com/cherhy/plugins/service/WriteVideoService.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/service/WriteVideoService.kt
@@ -1,11 +1,49 @@
 package com.cherhy.plugins.service
 
+import com.cherhy.common.util.model.UserId
+import com.cherhy.plugins.domain.*
 import com.cherhy.plugins.repository.VideoRepository
 
 class WriteVideoService(
     private val videoRepository: VideoRepository,
 ) {
-    suspend fun delete() {
-        TODO()
-    }
+    suspend fun create(
+        userId: UserId,
+        postId: PostId,
+        name: VideoName,
+        uniqueName: VideoUniqueName,
+        size: VideoSize,
+        extension: VideoExtension,
+    ) = videoRepository.save(
+        userId,
+        postId,
+        name,
+        uniqueName,
+        size,
+        extension,
+    )
+
+    suspend fun update(
+        videoId: VideoId,
+        userId: UserId,
+        name: VideoName,
+        uniqueName: VideoUniqueName,
+        size: VideoSize,
+        extension: VideoExtension,
+    ) = videoRepository.update(
+        videoId,
+        userId,
+        name,
+        uniqueName,
+        size,
+        extension,
+    )
+
+    suspend fun delete(
+        userId: UserId,
+        videoId: VideoId,
+    ) = videoRepository.delete(
+        userId,
+        videoId,
+    )
 }
