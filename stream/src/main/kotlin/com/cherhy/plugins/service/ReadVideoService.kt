9c92d9f4b699fcbc6ebf45c6d9a45e03845afe81
lolmageap <ekxk1234@naver.com>
2024-07-13 23:40:51 +0900

    implement read logic to video domain

--- stream/src/main/kotlin/com/cherhy/plugins/service/ReadVideoService.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/service/ReadVideoService.kt
@@ -1,11 +1,23 @@
 package com.cherhy.plugins.service
 
+import com.cherhy.plugins.domain.PostId
+import com.cherhy.plugins.domain.VideoId
 import com.cherhy.plugins.repository.VideoRepository
 
 class ReadVideoService(
     private val videoRepository: VideoRepository,
 ) {
-    suspend fun get() {
-        TODO()
-    }
+    suspend fun get(
+        videoId: VideoId,
+    ) =
+        videoRepository.find(
+            videoId,
+        )
+
+    suspend fun get(
+        postId: PostId,
+    ) =
+        videoRepository.find(
+            postId,
+        )
 }
