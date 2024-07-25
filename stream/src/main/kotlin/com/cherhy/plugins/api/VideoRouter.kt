1bce93b03b008c449695e1dbed7bda05c7058da4
lolmageap <ekxk1234@naver.com>
2024-07-25 22:31:53 +0900

    add action history to get video

--- stream/src/main/kotlin/com/cherhy/plugins/api/VideoRouter.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/api/VideoRouter.kt
@@ -1,6 +1,9 @@
 package com.cherhy.plugins.api
 
 import com.cherhy.common.util.Stream.Video.GET_VIDEO
+import com.cherhy.plugins.domain.Action
+import com.cherhy.plugins.domain.ActionHistory
+import com.cherhy.plugins.domain.actionHistory
 import com.cherhy.plugins.usecase.GetVideoUseCase
 import com.cherhy.plugins.util.ContentRangeGenerator
 import com.cherhy.plugins.util.extension.lastWatchedCheckpoint
@@ -12,9 +15,12 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
+import org.litote.kmongo.coroutine.CoroutineDatabase
+import java.time.ZonedDateTime
 
 fun Route.video() {
     val getVideoUseCase: GetVideoUseCase by inject()
+    val coroutineDatabase: CoroutineDatabase by inject()
 
     get(GET_VIDEO) {
         val userId = call.request.userId
@@ -34,6 +40,14 @@ fun Route.video() {
             video.currentSize,
             video.totalSize,
         )
+
+        val actionHistory = ActionHistory.of(
+            userId,
+            Action.SHOW_VIDEO,
+            ZonedDateTime.now(),
+        )
+
+        coroutineDatabase.actionHistory.insertOne(actionHistory)
 
         call.respondBytes(video.videoStream, ContentType.Video.MP4)
         call.respond(HttpStatusCode.OK)
