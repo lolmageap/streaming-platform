bab89dab0424fb44142e70ed31f85f8e9d6cb641
lolmageap <ekxk1234@naver.com>
2024-07-25 22:31:14 +0900

    setting reactive kmongo to dependency injection

--- stream/src/main/kotlin/com/cherhy/plugins/config/DependencyInjectionConfig.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/config/DependencyInjectionConfig.kt
@@ -12,9 +12,21 @@ import com.cherhy.plugins.usecase.*
 import io.ktor.server.application.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
+import org.litote.kmongo.coroutine.CoroutineClient
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun Application.configureDependencyInjection() {
+    val mongoUrl = environment.config.property("mongo.url").getString()
+    val mongoDatabase = environment.config.property("mongo.database").getString()
+
     install(Koin) {
+        modules(
+            module {
+                single { KMongo.createClient(mongoUrl).coroutine }
+                single { get<CoroutineClient>().getDatabase(mongoDatabase) }
+            }
+        )
         modules(dependencyInjectionModule)
     }
 }
