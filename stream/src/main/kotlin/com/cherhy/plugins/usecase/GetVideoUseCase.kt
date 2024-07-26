ff39dbd374077bd220cd1b2bea6296de6494e965
lolmageap <ekxk1234@naver.com>
2024-07-26 21:11:16 +0900

    refactor to coroutine database method

--- stream/src/main/kotlin/com/cherhy/plugins/usecase/GetVideoUseCase.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/usecase/GetVideoUseCase.kt
@@ -4,16 +4,18 @@ import com.cherhy.common.util.model.UserId
 import com.cherhy.plugins.api.GetVideoResponse
 import com.cherhy.plugins.config.MinioFactory
 import com.cherhy.plugins.config.reactiveTransaction
-import com.cherhy.plugins.domain.PostId
-import com.cherhy.plugins.domain.VideoId
+import com.cherhy.plugins.domain.*
 import com.cherhy.plugins.service.ReadVideoService
 import com.cherhy.plugins.util.ApplicationConfigUtils
 import com.cherhy.plugins.util.extension.download
 import com.cherhy.plugins.util.model.Bucket
 import com.cherhy.plugins.util.property.MinioProperty
+import org.litote.kmongo.coroutine.CoroutineDatabase
+import java.time.ZonedDateTime
 
 class GetVideoUseCase(
     private val readVideoService: ReadVideoService,
+    private val coroutineDatabase: CoroutineDatabase,
 ) {
     private val minioFactory = MinioFactory.newInstance()
 
@@ -36,6 +38,13 @@ class GetVideoUseCase(
                 video.extension,
                 lastVideoByte,
             ).readAllBytes()!!
+
+            val actionHistory = ActionHistory.of(
+                userId,
+                Action.SHOW_VIDEO,
+                ZonedDateTime.now(),
+            )
+            coroutineDatabase.actionHistory.insertOne(actionHistory)
 
             GetVideoResponse.of(videoStream, video.size)
         }
