ccadd77abdc6c155ff3204bfb27391d1a2a1240f
lolmageap <ekxk1234@naver.com>
2024-08-02 23:40:37 +0900

    add mongodb client to GetVideoUseCase

--- stream/src/main/kotlin/com/cherhy/plugins/DependencyInjectionConfig.kt
+++ stream/src/main/kotlin/com/cherhy/plugins/DependencyInjectionConfig.kt
@@ -1,14 +1,14 @@
-package com.cherhy.plugins.config
+package com.cherhy.plugins
 
-import com.cherhy.plugins.repository.PostRepository
-import com.cherhy.plugins.repository.PostRepositoryImpl
-import com.cherhy.plugins.repository.VideoRepository
-import com.cherhy.plugins.repository.VideoRepositoryImpl
-import com.cherhy.plugins.service.ReadPostService
-import com.cherhy.plugins.service.ReadVideoService
-import com.cherhy.plugins.service.WritePostService
-import com.cherhy.plugins.service.WriteVideoService
-import com.cherhy.plugins.usecase.*
+import com.cherhy.repository.PostRepository
+import com.cherhy.repository.PostRepositoryImpl
+import com.cherhy.repository.VideoRepository
+import com.cherhy.repository.VideoRepositoryImpl
+import com.cherhy.service.ReadPostService
+import com.cherhy.service.ReadVideoService
+import com.cherhy.service.WritePostService
+import com.cherhy.service.WriteVideoService
+import com.cherhy.usecase.*
 import io.ktor.server.application.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
@@ -36,7 +36,7 @@ val dependencyInjectionModule = module {
     single<GetPostUseCase> { GetPostUseCase(get()) }
     single<UpdatePostUseCase> { UpdatePostUseCase(get(), get(), get(), get()) }
     single<DeletePostUseCase> { DeletePostUseCase(get(), get(), get(), get()) }
-    single<GetVideoUseCase> { GetVideoUseCase(get()) }
+    single<GetVideoUseCase> { GetVideoUseCase(get(), get()) }
 
     single<ReadPostService> { ReadPostService(get()) }
     single<ReadVideoService> { ReadVideoService(get()) }
