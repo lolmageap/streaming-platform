1ca43869d87e5d305230bcd52d8ceedb51ad9cc3
lolmageap <ekxk1234@naver.com>
2024-08-17 23:17:34 +0900

    add time value

--- common/src/main/kotlin/com/cherhy/common/util/extension/ZonedDateTimeExtension.kt
+++ common/src/main/kotlin/com/cherhy/common/util/extension/ZonedDateTimeExtension.kt
@@ -1,6 +1,11 @@
 package com.cherhy.common.util.extension
 
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.ZoneOffset
+import java.time.ZoneOffset.UTC
 import java.time.ZonedDateTime
 
-fun ZonedDateTime.toSeoulTime() = withZoneSameInstant(ZoneOffset.ofHours(9))!!
+fun ZonedDateTime.toSeoulTime() = this.withZoneSameInstant(ZoneId.of("Asia/Seoul"))!!
+fun ZonedDateTime.toUtcTime() = this.withZoneSameInstant(ZoneId.of("UTC"))!!
+fun LocalDateTime.toZonedDateTime(zoneOffset: ZoneOffset = UTC) = ZonedDateTime.of(this, zoneOffset)!!
