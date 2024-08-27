2a76041e2f36e0ce7172b77bc8dc3920a674621d
lolmageap <ekxk1234@naver.com>
2024-08-27 17:39:39 +0900

    fix to zoned date time extension function

--- common/src/main/kotlin/com/cherhy/common/util/extension/ZonedDateTimeExtension.kt
+++ common/src/main/kotlin/com/cherhy/common/util/extension/ZonedDateTimeExtension.kt
@@ -6,6 +6,22 @@ import java.time.ZoneOffset
 import java.time.ZoneOffset.UTC
 import java.time.ZonedDateTime
 
-fun ZonedDateTime.toSeoulTime() = this.withZoneSameInstant(ZoneId.of("Asia/Seoul"))!!
-fun ZonedDateTime.toUtcTime() = this.withZoneSameInstant(ZoneId.of("UTC"))!!
-fun LocalDateTime.toZonedDateTime(zoneOffset: ZoneOffset = UTC) = ZonedDateTime.of(this, zoneOffset)!!
+fun ZonedDateTime.toSeoulTime(): ZonedDateTime =
+    when(this.zone) {
+        UTC -> this.plusHours(9)
+        SEOUL -> this
+        else -> this.withZoneSameInstant(SEOUL)
+    }
+
+fun ZonedDateTime.toUtcTime(): ZonedDateTime =
+    when(this.zone) {
+        UTC -> this
+        SEOUL -> this.minusHours(9)
+        else -> this.withZoneSameInstant(UTC)
+    }
+
+fun LocalDateTime.toZonedDateTime(
+    zoneOffset: ZoneOffset = UTC,
+) = ZonedDateTime.of(this, zoneOffset)!!
+
+private val SEOUL = ZoneId.of("Asia/Seoul")
