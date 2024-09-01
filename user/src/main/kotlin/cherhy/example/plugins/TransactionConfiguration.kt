7b9535b201440759f4ddfcc02b8469f05e583576
lolmageap <ekxk1234@naver.com>
2024-09-01 17:39:59 +0900

    setting to read only reactive transaction

--- user/src/main/kotlin/cherhy/example/plugins/TransactionConfiguration.kt
+++ user/src/main/kotlin/cherhy/example/plugins/TransactionConfiguration.kt
@@ -3,15 +3,38 @@ package cherhy.example.plugins
 import cherhy.example.util.DatabaseType
 import cherhy.example.util.TransactionType
 import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.statements.StatementContext
+import org.jetbrains.exposed.sql.statements.StatementInterceptor
+import org.jetbrains.exposed.sql.statements.StatementType
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 suspend fun <T> reactiveTransaction(
     transactionType: TransactionType = TransactionType.WRITE,
     block: suspend () -> T,
-): T {
-    val database =
-        if (transactionType == TransactionType.WRITE) DatabaseType.masterDatabase
-        else DatabaseType.slaveDatabase
+): T =
+    if (transactionType == TransactionType.WRITE) {
+        newSuspendedTransaction(Dispatchers.IO, DatabaseType.masterDatabase) {
+            block()
+        }
+    } else {
+        newSuspendedTransaction(Dispatchers.IO, DatabaseType.slaveDatabase) {
+            TransactionManager.current().registerInterceptor(TransactionInterceptor)
+            block()
+        }
+    }
 
-    return newSuspendedTransaction(Dispatchers.IO, database) { block() }
+private object TransactionInterceptor : StatementInterceptor {
+    override fun beforeExecution(
+        transaction: Transaction,
+        context: StatementContext,
+    ) =
+        when (context.statement.type) {
+            StatementType.INSERT,
+            StatementType.UPDATE,
+            StatementType.DELETE,
+            -> throw IllegalStateException("Read-only transaction is not allowed to execute write operations.")
+            else -> Unit
+        }
 }
