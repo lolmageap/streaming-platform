a23a455e3f6b8cc25d25d64791c3ff7ec8f729cf
lolmageap <ekxk1234@naver.com>
2024-10-06 22:36:27 +0900

    remove jdsl dependency and query

--- payment/src/main/kotlin/com/cherhy/payment/adapter/out/persistence/TestCoroutineRepository.kt
+++ payment/src/main/kotlin/com/cherhy/payment/adapter/out/persistence/TestCoroutineRepository.kt
@@ -1,14 +1,11 @@
 package com.cherhy.payment.adapter.out.persistence
 
-import com.linecorp.kotlinjdsl.querydsl.expression.col
-import com.linecorp.kotlinjdsl.spring.data.SpringDataQueryFactory
-import com.linecorp.kotlinjdsl.spring.data.pageQuery
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
 
-interface TestCoroutineRepository : CoroutineCrudRepository<TestR2dbcEntity, Long>, TestRepositoryCustom {
-}
+interface TestCoroutineRepository : CoroutineCrudRepository<TestR2dbcEntity, Long>, TestRepositoryCustom
 
 interface TestRepositoryCustom {
     suspend fun findAll(
@@ -18,25 +15,25 @@ interface TestRepositoryCustom {
     ): Page<TestR2dbcEntity>
 }
 
-class TestRepositoryCustomImpl(
-    private val sessionFactory: SpringDataQueryFactory,
-) : TestRepositoryCustom {
+@Repository
+class TestRepositoryCustomImpl : TestRepositoryCustom {
     override suspend fun findAll(
         name: String?,
         status: String?,
         pageable: Pageable
     ): Page<TestR2dbcEntity> {
-        return sessionFactory.pageQuery(pageable) {
-            select(entity(TestR2dbcEntity::class))
-            from(entity(TestR2dbcEntity::class))
-            whereAnd(
-                name?.let {
-                    col(TestR2dbcEntity::name).equal(name)
-                },
-                status?.let {
-                    col(TestR2dbcEntity::status).equal(status)
-                },
-            )
-        }
+        TODO("Not yet implemented")
+//        return sessionFactory.pageQuery(pageable) {
+//            select(entity(TestR2dbcEntity::class))
+//            from(entity(TestR2dbcEntity::class))
+//            whereAnd(
+//                name?.let {
+//                    col(TestR2dbcEntity::name).equal(name)
+//                },
+//                status?.let {
+//                    col(TestR2dbcEntity::status).equal(status)
+//                },
+//            )
+//        }
     }
 }
