aa1ad255439560a3fb4b7e973832b37b253de1ff
lolmageap <ekxk1234@naver.com>
2024-10-06 22:33:33 +0900

    disable to test profile

--- payment/src/main/kotlin/com/cherhy/payment/config/JasyptConfig.kt
+++ payment/src/main/kotlin/com/cherhy/payment/config/JasyptConfig.kt
@@ -3,11 +3,13 @@ package com.cherhy.payment.config
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 // TODO: docker 환경 변수로 알고리즘과 비밀번호 설정해야함.
 @Configuration
 class JasyptConfig {
     @Bean
+    @Profile("!test")
     fun jasyptStringEncryptor() =
         StandardPBEStringEncryptor().apply {
             setAlgorithm(
