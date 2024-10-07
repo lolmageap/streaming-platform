4609a0f5270f6e52109c8e52d7bc5f607f1f1ad9
lolmageap <ekxk1234@naver.com>
2024-10-07 23:43:54 +0900

    create third party constant

---
+++ stream/src/main/kotlin/com/cherhy/util/constant/CacheConst.kt
@@ -0,0 +1,6 @@
+package com.cherhy.util.constant
+
+object CacheConst {
+    const val REDIS_URL = "redis.url"
+    const val REDIS_PORT = "redis.port"
+}
---
+++ stream/src/main/kotlin/com/cherhy/util/constant/MongoConst.kt
@@ -0,0 +1,6 @@
+package com.cherhy.util.constant
+
+object MongoConst {
+    const val MONGO_URL = "mongo.url"
+    const val MONGO_DATABASE = "mongo.database"
+}
