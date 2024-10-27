19d608854de8f38160b7796ece8c7302ffa7fc8e
lolmageap <ekxk1234@naver.com>
2024-10-27 19:38:20 +0900

    implement string redis template extension

---
+++ payment/src/main/kotlin/com/cherhy/payment/util/lib/StringRedisTemplate.kt
@@ -0,0 +1,12 @@
+package com.cherhy.payment.util.lib
+
+import org.springframework.data.redis.core.StringRedisTemplate
+
+fun StringRedisTemplate.get(
+    key: String,
+): String = opsForValue().get(key)!!
+
+fun StringRedisTemplate.set(
+    key: String,
+    value: String,
+) = opsForValue().set(key, value)
