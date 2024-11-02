385e10184bb38203be40bed179901d317926ca2f
lolmageap <ekxk1234@naver.com>
2024-11-02 18:43:43 +0900

    exclude r2dbc auto configuration

--- payment/src/main/kotlin/com/cherhy/payment/PaymentApplication.kt
+++ payment/src/main/kotlin/com/cherhy/payment/PaymentApplication.kt
@@ -1,13 +1,17 @@
 package com.cherhy.payment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["com.cherhy.producer"])
+@SpringBootApplication(
+    scanBasePackages = ["com.cherhy.producer"],
+    exclude = [R2dbcAutoConfiguration::class],
+)
 @ConfigurationPropertiesScan("com.cherhy.payment.util.property")
-class ExampleApplication
+class PaymentApplication
 
 fun main(args: Array<String>) {
-    runApplication<ExampleApplication>(*args)
+    runApplication<PaymentApplication>(*args)
 }
--- payment/src/main/kotlin/com/cherhy/payment/util/property/R2dbcDataSourceProperty.kt
+++ payment/src/main/kotlin/com/cherhy/payment/util/property/R2dbcDataSourceProperty.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "r2dbc")
 data class R2dbcDataSourceProperty(
-    val slave: R2dbcProperty,
     val master: R2dbcProperty,
+    val slave: R2dbcProperty,
 )
 
 data class R2dbcProperty(
