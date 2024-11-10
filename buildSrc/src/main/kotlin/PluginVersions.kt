2e7ff6d7eb3b12be4ac66e7a47adb7b9f00fbf05
lolmageap <ekxk1234@naver.com>
2024-11-10 20:42:10 +0900

    create plugins

---
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -0,0 +1,8 @@
+object PluginVersions {
+    const val KOTLIN_VERSION = "2.0.0"
+    const val KTOR_PLUGIN_VERSION = "2.3.11"
+    const val SPRING_BOOT_VERSION = "3.2.5"
+    const val SHADOW_JAR_VERSION = "8.1.1"
+    const val DEPENDENCY_MANAGEMENT_VERSION = "1.1.4"
+    const val DOCKER_PLUGIN_VERSION = "0.36.0"
+}
---
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -0,0 +1,10 @@
+object Plugins {
+    const val JVM = "jvm"
+    const val KTOR_PLUGIN = "io.ktor.plugin"
+    const val SPRING_BOOT = "org.springframework.boot"
+    const val DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
+    const val SHADOW_JAR = "com.github.johnrengelman.shadow"
+    const val DOCKER_PLUGIN = "com.palantir.docker"
+
+    const val KOTLIN_SPRING = "kotlin-spring"
+}
