12ea7c1752a4cf2d59a7eabdeaee95d19b4f8d34
lolmageap <ekxk1234@naver.com>
2024-11-10 20:44:43 +0900

    add no arg, all open plugin

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -1,10 +1,11 @@
 object Plugins {
     const val JVM = "jvm"
-    const val KTOR_PLUGIN = "io.ktor.plugin"
-    const val SPRING_BOOT = "org.springframework.boot"
-    const val DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
+    const val NO_ARG = "plugin.noarg"
+    const val ALL_OPEN = "plugin.allopen"
     const val SHADOW_JAR = "com.github.johnrengelman.shadow"
-    const val DOCKER_PLUGIN = "com.palantir.docker"
-
+    const val SPRING_BOOT = "org.springframework.boot"
+    const val KTOR_PLUGIN = "io.ktor.plugin"
     const val KOTLIN_SPRING = "kotlin-spring"
+    const val DOCKER_PLUGIN = "com.palantir.docker"
+    const val DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
 }
