0a692df47c6e92a4a1e60c3e8c355bfa7013eaf7
lolmageap <ekxk1234@naver.com>
2024-11-12 22:14:35 +0900

    fix cache serializer

--- payment/src/main/kotlin/com/cherhy/payment/config/CacheConfig.kt
+++ payment/src/main/kotlin/com/cherhy/payment/config/CacheConfig.kt
@@ -1,9 +1,6 @@
 package com.cherhy.payment.config
 
 import com.cherhy.payment.util.property.CacheProperty
-import mu.KotlinLogging
-import org.springframework.cache.Cache
-import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,8 +8,11 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
 
 @EnableCaching
 @Configuration
@@ -20,42 +20,32 @@ class CacheConfig(
     private val cacheProperty: CacheProperty,
 ) {
     @Bean
+    fun stringRedisTemplate() =
+        StringRedisTemplate(redisConnectionFactory())
+            .apply {
+                valueSerializer = GenericJackson2JsonRedisSerializer()
+            }
+
+    @Bean
     fun redisConnectionFactory() =
         LettuceConnectionFactory(
             RedisStandaloneConfiguration(cacheProperty.host, cacheProperty.port)
         )
 
     @Bean
-    fun localCacheManager(): CacheManager {
-        return object : CacheManager {
-            private val logger = KotlinLogging.logger {}
+    fun redisCacheManager() =
+        RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(redisConnectionFactory())
+            .cacheDefaults(defaultConfiguration())
+            .build()
 
-            val cacheConfiguration =
-                RedisCacheConfiguration
-                    .defaultCacheConfig()
-                    .serializeValuesWith(
-                        fromSerializer(
-                            Jackson2JsonRedisSerializer(Any::class.java)
-                        )
-                    )
-
-            private val delegate =
-                RedisCacheManager
-                    .builder(redisConnectionFactory())
-                    .cacheDefaults(cacheConfiguration)
-                    .enableCreateOnMissingCache()
-                    .build()
-
-            override fun getCache(
-                name: String,
-            ): Cache? =
-                delegate.getCache(name).also {
-                    if (it != null) logger.info { "Cache Hit : $name" }
-                    else logger.info { "Cache Miss : $name" }
-                }
-
-            override fun getCacheNames() =
-                delegate.cacheNames
-        }
-    }
+    private fun defaultConfiguration() =
+        RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+            .entryTtl(Duration.ofMinutes(10))
 }
