42c6b7194081dbe4b57bebeb510106825b872c7e
lolmageap <ekxk1234@naver.com>
2024-12-23 21:08:49 +0900

    setting for component scan and exclude auto configuration

--- payment/src/main/kotlin/com/cherhy/payment/PaymentApplication.kt
+++ payment/src/main/kotlin/com/cherhy/payment/PaymentApplication.kt
@@ -1,13 +1,20 @@
 package com.cherhy.payment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(
-    scanBasePackages = ["com.cherhy.producer"],
-    exclude = [R2dbcAutoConfiguration::class],
+    scanBasePackages = [
+        "com.cherhy.producer",
+        "com.cherhy.payment",
+    ],
+    exclude = [
+        R2dbcAutoConfiguration::class,
+        DataSourceAutoConfiguration::class,
+    ],
 )
 @ConfigurationPropertiesScan("com.cherhy.payment.util.property")
 class PaymentApplication
