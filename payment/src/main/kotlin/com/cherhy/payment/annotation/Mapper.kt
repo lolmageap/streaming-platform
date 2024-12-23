da6943bf7fdbd23b85c4139997b89d36f70fe8f6
lolmageap <ekxk1234@naver.com>
2024-12-23 20:52:16 +0900

    refactor layer annotation

--- payment/src/main/kotlin/com/cherhy/payment/annotation/Mapper.kt
+++ payment/src/main/kotlin/com/cherhy/payment/annotation/Mapper.kt
@@ -1,9 +1,13 @@
 package com.cherhy.payment.annotation
 
+import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Component
 
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @Component
-annotation class Mapper
+annotation class Mapper(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+)
--- payment/src/main/kotlin/com/cherhy/payment/annotation/UseCase.kt
+++ payment/src/main/kotlin/com/cherhy/payment/annotation/UseCase.kt
@@ -1,9 +1,13 @@
 package com.cherhy.payment.annotation
 
+import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Component
 
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @Component
-annotation class UseCase
+annotation class UseCase(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+)
