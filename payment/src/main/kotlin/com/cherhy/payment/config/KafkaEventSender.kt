5509fd82db93480f62efcc381e0e2678bf615b86
lolmageap <ekxk1234@naver.com>
2024-12-28 23:07:28 +0900

    implement kafka event sender

--- payment/src/main/kotlin/com/cherhy/payment/config/KafkaEventSender.kt
+++ payment/src/main/kotlin/com/cherhy/payment/config/KafkaEventSender.kt
@@ -1,7 +1,9 @@
 package com.cherhy.payment.config
 
+import com.cherhy.common.util.KafkaConstant.Topic.PURCHASE_VIDEO_TOPIC
 import com.cherhy.common.util.KafkaConstant.Topic.TEST_TOPIC
-import com.cherhy.payment.util.model.PaymentEvent
+import com.cherhy.common.util.event.PaymentEvent
+import com.cherhy.common.util.event.PurchaseVideoEvent
 import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KotlinLogging
 import org.springframework.kafka.core.KafkaTemplate
@@ -14,12 +16,24 @@ class KafkaEventSender(
 ) {
     private val logger = KotlinLogging.logger {}
 
-    fun send(
+    suspend fun send(
         event: PaymentEvent,
     ) {
         val jsonToModel = objectMapper.writeValueAsString(event)
         try {
             kafkaTemplate.send(TEST_TOPIC, jsonToModel)
+        } catch (e: Exception) {
+            logger.error { "Error sending message to Kafka: ${e.message}" }
+        }
+        logger.info { "Message sent to Kafka: $jsonToModel" }
+    }
+
+    suspend fun send(
+        event: PurchaseVideoEvent,
+    ) {
+        val jsonToModel = objectMapper.writeValueAsString(event)
+        try {
+            kafkaTemplate.send(PURCHASE_VIDEO_TOPIC, jsonToModel)
         } catch (e: Exception) {
             logger.error { "Error sending message to Kafka: ${e.message}" }
         }
