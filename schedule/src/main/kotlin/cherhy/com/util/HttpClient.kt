f2ddd655aa2849f4ab6e9da0f74cab68e535f94f
lolmageap <ekxk1234@naver.com>
2024-12-30 21:45:10 +0900

    implement http client

--- schedule/src/main/kotlin/cherhy/com/util/HttpClient.kt
+++ schedule/src/main/kotlin/cherhy/com/util/HttpClient.kt
@@ -6,23 +6,34 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.util.*
+import io.ktor.util.reflect.*
 
 val httpClient
     get() = HttpClient(CIO)
 
 @OptIn(InternalAPI::class)
-suspend inline fun <reified T : Any> HttpClient.call(
+suspend inline fun <reified RESPONSE_TYPE> HttpClient.call(
     url: String,
     httpMethod: HttpMethod,
-    body: Any? = null,
-): T =
+    requestHeader: Map<String, String>? = null,
+    requestBody: Any? = null,
+) =
     this.use {
-        this.get(url) {
+        this.request(url) {
             method = httpMethod
-
+            requestHeader?.forEach { (key, value) ->
+                this.headers {
+                    append(key, value)
+                }
+            }
             if (httpMethod == HttpMethod.Post || httpMethod == HttpMethod.Put) {
-                contentType(ContentType.Application.Json)
-                if (body != null) this.body = body
+                if (requestBody != null) {
+                    this.body = requestBody
+                    this.bodyType = typeInfo<LinkedHashMap<String, Any>>()
+                    this.headers {
+                        append(HttpHeaders.ContentType, ContentType.Application.Json)
+                    }
+                }
             }
         }
-    }.body<T>()
+    }.body<RESPONSE_TYPE>()
