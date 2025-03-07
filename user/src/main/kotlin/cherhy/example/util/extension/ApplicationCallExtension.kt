5dd05e1ce04ab57b34ad4595bdb6bdf4fe86b313
lolmageap <ekxk1234@naver.com>
2025-03-07 23:52:13 +0900

    refactor query parameter handling in ApplicationCallExtension

--- user/src/main/kotlin/cherhy/example/util/extension/ApplicationCallExtension.kt
+++ user/src/main/kotlin/cherhy/example/util/extension/ApplicationCallExtension.kt
@@ -1,16 +1,14 @@
 package cherhy.example.util.extension
 
+import cherhy.example.plugins.objectMapperKey
 import cherhy.example.util.PathVariable
 import com.cherhy.common.util.USER_ID
 import com.cherhy.common.util.model.toUserId
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import java.nio.file.AccessDeniedException
-
-val mapper = jacksonObjectMapper()
 
 val ApplicationCall.pathVariable
     get() = PathVariable(this)
@@ -19,16 +17,17 @@ val ApplicationCall.jwt
     get() = this.principal<JWTPrincipal>()
         ?: throw AccessDeniedException("Invalid token")
 
-inline fun <reified T : Any> ApplicationCall.getQueryParams(): T {
-    return this.request.queryParameters.toClass()
-}
+inline fun <reified T : Any> ApplicationCall.modelAttribute(): T {
+    val objectmapper = application.attributes[objectMapperKey]
 
-inline fun <reified T : Any> Parameters.toClass(): T {
-    val map = this.entries().associate {
-        it.key to (it.value.getOrNull(0)
-            ?: throw IllegalArgumentException("Missing value for key ${it.key}"))
-    }
-    return mapper.convertValue(map, T::class.java)
+    val map =
+        request.queryParameters.entries().associate { (key, values) ->
+            if (values.isEmpty()) key to null
+            else if (values.size == 1) key to values.first()
+            else key to values
+        }
+
+    return objectmapper.convertValue(map, T::class.java)
 }
 
 val Headers.userId
