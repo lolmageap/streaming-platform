730ca99ac3f1d86ec17e4d637819119cdf0d5212
lolmageap <ekxk1234@naver.com>
2025-03-07 23:51:54 +0900

    configure Jackson serialization and deserialization settings

--- user/src/main/kotlin/cherhy/example/plugins/JacksonConfig.kt
+++ user/src/main/kotlin/cherhy/example/plugins/JacksonConfig.kt
@@ -1,11 +1,27 @@
 package cherhy.example.plugins
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.util.*
+
+val objectMapperKey = AttributeKey<ObjectMapper>("ObjectMapper")
 
 fun Application.configureJackson() {
     install(ContentNegotiation) {
-        jackson()
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            registerModules(JavaTimeModule())
+            registerKotlinModule()
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+            this@configureJackson.attributes.put(objectMapperKey, this)
+        }
     }
 }
